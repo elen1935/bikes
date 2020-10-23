58c83df88e18bca837061432e1aa1b52a3befd00
elen1935 <elen1935@stud.kea.dk>
2020-10-23 15:15:35 +0200

    data fetch

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ fetch("http://efcreations.es/t9w1/wp-json/wp/v2/bike")
 
 function handleData(posts) {
     //we now have the JSON. I could have also called posts, data
-    console.log(posts)
+    /*console.log(posts)*/
 
     //loop through theme
-    //posts.forEach(showPost)
+    posts.forEach(showPost)
 }
 
 function showPost(post) {
     console.log(post);
     //template cloning
-    const template = document.querySelector("template").content;
+    const template = document.querySelector("template#bikeTemplate").content;
     const copy = template.cloneNode(true);
 
-    copy.querySelector("h4").textContent=post.model;
+    copy.querySelector("h4").textContent = post.model;
+    copy.querySelector(".price").textContent = post.price;
     /*copy.querySelector(".content").innerHTML=post.content.rendered;*/
 
 
