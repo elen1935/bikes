25d803280c85ba42b517bb7d0f848c13d9c82eda
elen1935 <elen1935@stud.kea.dk>
2020-10-25 13:57:27 +0100

    fixed data fetch

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 fetch("http://efcreations.es/t9w1/wp-json/wp/v2/bike")
-    .then(res => res.json())
-    .then(handleData)
+    .then(initial => initial.json())
+    .then(callback);
 
-function handleData(posts) {
-    //we now have the JSON. I could have also called posts, data
-    /*console.log(posts)*/
+function callback(data) {
+    console.log(data)
 
-    //loop through theme
-    posts.forEach(showPost)
+    data.forEach(showPost)
 }
 
 function showPost(post) {
-    console.log(post);
-    //template cloning
+    console.log(post)
     const template = document.querySelector("template#bikeTemplate").content;
-    const copy = template.cloneNode(true);
+    const clone = template.cloneNode(true);
 
-    copy.querySelector("h4").textContent = post.model;
-    copy.querySelector(".price").textContent = "$" + post.price;
+    clone.querySelector(".model").textContent = post.model;
+    clone.querySelector(".price").textContent = "$" + post.price;
+
+
     /*copy.querySelector(".colour").innerHTML = post.content.colour;*/
    /*copy.querySelector(".in-stock").innerHTML = post.content.in_stock;*/
 
 
     /*copy.querySelector(".content").innerHTML = post.content.rendered;*/
 
-
-    //appending
-    document.querySelector("main").appendChild(copy);
+    document.querySelector("main").appendChild(clone);
 }
