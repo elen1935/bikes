e56f463b251a7a774c5347365c0fb9942751e71e
elen1935 <elen1935@stud.kea.dk>
2020-10-25 23:44:14 +0100

    clone rest of fields

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ function showPost(post) {
     const template = document.querySelector("template#bikeTemplate").content;
     const clone = template.cloneNode(true);
 
+    clone.querySelector(".brand").textContent = post.bike_brand;
     clone.querySelector(".model").textContent = post.model;
     clone.querySelector(".price").textContent = "$" + post.price;
+    clone.querySelector(".colour").textContent = post.colour;
+    clone.querySelector(".in-stock").textContent = post.in_stock;
 
-
-    /*copy.querySelector(".colour").innerHTML = post.content.colour;*/
-   /*copy.querySelector(".in-stock").innerHTML = post.content.in_stock;*/
     /*copy.querySelector(".content").innerHTML = post.content.rendered;*/
 
     document.querySelector("main").appendChild(clone);
